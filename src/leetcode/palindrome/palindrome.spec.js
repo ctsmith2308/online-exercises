70f831fee26e78b94585e8595faf3e649ebe8d29
Christopher Smith <ctsmith2308@gmail.com>
2019-10-27 17:25:21 -0600

    add spec files for palindrome and twoSums challenge

---
+++ src/leetcode/palindrome/palindrome.spec.js
@@ -0,0 +1,5 @@
+describe('Should run the tests for palindrome challenge', () =>{
+    it('Should run the first test', () => { 
+        console.log('I passed!')
+    })
+})
---
+++ src/leetcode/twoSums/twoSums.spec.js
@@ -0,0 +1,5 @@
+describe('Should run the tests for twoSums challenge', () =>{
+    it('Should run the first test', () => { 
+        console.log('I passed!')
+    })
+})
