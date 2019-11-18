7a5558e347780c4fafabe7f3603ab3cfb1b9fe21
Christopher Smith <ctsmith2308@gmail.com>
2019-11-17 17:30:42 -0700

    created snail js and spec file

---
+++ src/codeWars/snail/snail.js
@@ -0,0 +1,19 @@
+const snail = (arr, completeArray = []) => {
+  let [top, middle, bottom, end] = [[], [], [], []];
+  for(let i = 0; i <= arr.length; i ++) {
+    if(i === 0){
+     top.push(...arr[0]);
+    } else if (i === arr.length - 1) {
+     bottom.push(...arr[arr.length - 1])
+    } else if(arr[i]) {
+     let subArr = arr[i];
+     middle.push(...subArr.splice(subArr.length-1, 1))
+     end.unshift(...subArr.splice(0, 1));
+    }
+   }
+   let remainderArr = arr.slice(1, arr.length - 1);
+   let concatedArr = completeArray.concat(top, middle, bottom.reverse(), end);
+   return remainderArr.length >= 1? snail(remainderArr, concatedArr) : concatedArr
+ }
+ 
+//  snail(testArr)
